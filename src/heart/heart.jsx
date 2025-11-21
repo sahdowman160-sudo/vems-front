50cec44d332de909d62f183adc2b70494385d640
sahdowman160-sudo <sahdowman160@gmail.com>
2025-11-21 03:36:07 +0200

    Update API endpoints to use production URLs

--- src/heart/heart.jsx
+++ src/heart/heart.jsx
@@ -15,7 +15,7 @@ const AddInspoPage = () => {
       try {
         const token = window.localStorage.getItem("token");
         
-        const response = await fetch('http://127.0.0.1:8000/like', {
+        const response = await fetch('https://vems-api.yousseif.me/like', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -115,7 +115,7 @@ const AddInspoPage = () => {
     try {
       const token = window.localStorage.getItem("token");
       
-      const response = await fetch('http://127.0.0.1:8000/delete_like', {
+      const response = await fetch('https://vems-api.yousseif.me/delete_like', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
