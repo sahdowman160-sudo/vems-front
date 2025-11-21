7078e06e1e77939710c8fb6ebc79848d0a5669ac
sahdowman160-sudo <sahdowman160@gmail.com>
2025-11-21 03:57:17 +0200

    Update API endpoints to use production URLs

--- src/admin/Insert_proudect.jsx
+++ src/admin/Insert_proudect.jsx
@@ -57,7 +57,7 @@ const navigate = useNavigate();
       }
 
       try {
-        const response = await fetch("http://127.0.0.1:8000/extrct_super", {
+        const response = await fetch("https://vems-api.yousseif.me/extrct_super", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -171,7 +171,7 @@ navigate("/login");
         data.append("images", img.file);
       });
 
-      const response = await fetch("http://127.0.0.1:8000/insert_proudect", {
+      const response = await fetch("https://vems-api.yousseif.me/insert_proudect", {
         method: "POST",
         body: data,
       });
