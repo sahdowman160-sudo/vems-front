33e170153182625bc1f9413e97d548e2a26fdfc8
sahdowman160-sudo <sahdowman160@gmail.com>
2025-12-15 16:17:29 +0200

    Update change_profile.jsx

--- src/profile/change_profile.jsx
+++ src/profile/change_profile.jsx
@@ -80,7 +80,7 @@ export default function ChangePassword() {
       // Get token from localStorage
       const token = localStorage.getItem('token');
       
-      const response = await fetch('https://vems-api.yousseif.me/change-password', {
+      const response = await fetch('https://kenzy-api.usif.space/change-password', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
