95008c4a610421cd1a86846b352999add239cb05
sahdowman160-sudo <sahdowman160@gmail.com>
2025-12-15 16:14:11 +0200

    Update register.jsx

--- src/login/register.jsx
+++ src/login/register.jsx
@@ -13,7 +13,7 @@ const [isLoading, setIsLoading] = useState(false);
     e.preventDefault();
     setIsLoading(true);
     try {
-      const response = await fetch("http://127.0.0.1:8000/register", {
+      const response = await fetch("https://kenzy-api.usif.space/ register", {
         
         method: "POST",
             headers: {
