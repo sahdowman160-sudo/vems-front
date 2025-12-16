b7ab6ee6e3c9d27318f49464d7264b2120539224
sahdowman160-sudo <sahdowman160@gmail.com>
2025-12-16 15:12:30 +0200

    Update cart.jsx

--- src/details_proudact/cart.jsx
+++ src/details_proudact/cart.jsx
@@ -649,8 +649,9 @@ const DetailsProduct = () => {
               </Link>
             </button>
             <button className="p-2 hover:bg-gray-800 rounded-full transition-colors relative bg-white/10 backdrop-blur-sm border border-white/20">
+              <Link to="/cart">
               <ShoppingCart className="w-5 h-5 text-gray-400" />
-              <span className="absolute -top-1 -right-1 w-4 h-4 bg-purple-500 rounded-full text-xs flex items-center justify-center text-white">2</span>
+                  </Link>
             </button>
           </div>
         </div>
