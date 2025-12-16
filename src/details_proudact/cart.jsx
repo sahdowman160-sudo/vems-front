52030bfe0429499265a0a39f0271f56eeb4e4018
sahdowman160-sudo <sahdowman160@gmail.com>
2025-12-16 15:22:39 +0200

    Update cart.jsx

--- src/details_proudact/cart.jsx
+++ src/details_proudact/cart.jsx
@@ -674,7 +674,6 @@ const DetailsProduct = () => {
                   <Link to="/heart">
                     <Heart className="w-5 h-5 text-gray-400" />
                   </Link>
-                  <span className="absolute -top-1 -right-1 w-4 h-4 bg-purple-500 rounded-full text-xs flex items-center justify-center text-white">3</span>
                 </button>
               </div>
             </nav>
